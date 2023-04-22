2bfd7b0b4893ee8d8026b76a0a0b8bd2cc99a26c
Sumit Jaiswal <jaiswalsumit18@gmail.com>
2023-04-22 17:28:41 +0100

    Inject Todo service and assing data to completed and todo component

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoService } from '../shared/services/todo.service';
+import { Observable } from 'rxjs';
+import { Todo } from '../model/todo';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.scss']
+  styleUrls: ['./todo.component.scss'],
 })
 export class TodoComponent implements OnInit {
+  todoList$: Observable<Todo[]> | undefined;
+  completedList$: Observable<Todo[]> | undefined;
 
-  constructor() { }
+  constructor(private todoService: TodoService) {}
 
   ngOnInit(): void {
-  }
+    this.todoService.getTodoList();
 
+    this.todoList$ = this.todoService.filterByStatus(false);
+    this.completedList$ = this.todoService.filterByStatus(true);
+  }
 }
