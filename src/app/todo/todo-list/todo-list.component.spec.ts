8fa5c543ec2fa0b4864e00aedc39204fceb18afc
Sumit Jaiswal <jaiswalsumit18@gmail.com>
2023-04-22 19:30:11 +0100

    Write unit test cases for todo list  component

--- src/app/todo/todo-list/todo-list.component.spec.ts
+++ src/app/todo/todo-list/todo-list.component.spec.ts
@@ -3,9 +3,12 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { TodoListComponent } from './todo-list.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { HttpClientModule } from '@angular/common/http';
+import { TODOS_LIST } from 'src/app/testing/todos.mock';
+import { TodoService } from 'src/app/shared/services/todo.service';
 
 describe('TodoListComponent', () => {
   let component: TodoListComponent;
+  let todoService: TodoService;
   let fixture: ComponentFixture<TodoListComponent>;
 
   beforeEach(async () => {
@@ -18,10 +21,19 @@ describe('TodoListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(TodoListComponent);
     component = fixture.componentInstance;
+    todoService = TestBed.inject(TodoService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should called markCompleted on #onItemCheck', () => {
+    let todoItem = TODOS_LIST.todos[0];
+    let spyMarkCompleted = spyOn(todoService, 'markCompleted');
+    component.onItemCheck(todoItem);
+
+    expect(spyMarkCompleted).toHaveBeenCalledWith(todoItem);
   });
 });
