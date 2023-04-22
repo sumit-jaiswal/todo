67dc190ff44eb2c0c7891494810d9ea965efd44e
Sumit Jaiswal <jaiswalsumit18@gmail.com>
2023-04-22 17:29:03 +0100

    Add a input and display value in completed

--- src/app/todo/completed-list/completed-list.component.ts
+++ src/app/todo/completed-list/completed-list.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Todo } from 'src/app/model/todo';
 
 @Component({
   selector: 'app-completed-list',
   templateUrl: './completed-list.component.html',
-  styleUrls: ['./completed-list.component.scss']
+  styleUrls: ['./completed-list.component.scss'],
 })
 export class CompletedListComponent implements OnInit {
+  @Input() public todos: Todo[] | undefined;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
