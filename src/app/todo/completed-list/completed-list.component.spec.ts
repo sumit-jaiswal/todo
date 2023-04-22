387e23aad7d251e0bfe0c130138d211ba52d4a76
Sumit Jaiswal <jaiswalsumit18@gmail.com>
2023-04-22 16:29:36 +0100

    Creates completed-list component

---
+++ src/app/todo/completed-list/completed-list.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CompletedListComponent } from './completed-list.component';
+
+describe('CompletedListComponent', () => {
+  let component: CompletedListComponent;
+  let fixture: ComponentFixture<CompletedListComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CompletedListComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CompletedListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/todo/todo.module.ts
+++ src/app/todo/todo.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { TodoRoutingModule } from './todo-routing.module';
 import { TodoComponent } from './todo.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
+import { CompletedListComponent } from './completed-list/completed-list.component';
 
 @NgModule({
-  declarations: [TodoComponent, TodoListComponent],
+  declarations: [TodoComponent, TodoListComponent, CompletedListComponent],
   imports: [CommonModule, TodoRoutingModule],
 })
 export class TodoModule {}
