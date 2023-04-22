e17f95e42f94e2db87973072933d2f7dced351c7
Sumit Jaiswal <jaiswalsumit18@gmail.com>
2023-04-22 16:20:13 +0100

    Create Todo module

---
+++ src/app/todo/todo.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TodoRoutingModule } from './todo-routing.module';
+import { TodoComponent } from './todo.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+
+
+@NgModule({
+  declarations: [
+    TodoComponent,
+    TodoListComponent
+  ],
+  imports: [
+    CommonModule,
+    TodoRoutingModule
+  ]
+})
+export class TodoModule { }
