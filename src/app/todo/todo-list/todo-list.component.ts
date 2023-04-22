f24da0eeaf46d9ff9cbe29a60dfa617572a2833b
Sumit Jaiswal <jaiswalsumit18@gmail.com>
2023-04-22 17:29:32 +0100

    Adds Input and display data in todo component

--- src/app/todo/todo-list/todo-list.component.ts
+++ src/app/todo/todo-list/todo-list.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Todo } from 'src/app/model/todo';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.scss']
+  styleUrls: ['./todo-list.component.scss'],
 })
 export class TodoListComponent implements OnInit {
+  @Input() public todos: Todo[] | undefined;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onItemCheck(todo: Todo) {
+    console.log(todo);
   }
-
 }
